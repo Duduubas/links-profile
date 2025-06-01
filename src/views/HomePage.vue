30b17c27c5eb95f83efaaba45cd6332c05912440
Duduubas <eduardobritogomes70@gmail.com>
2025-05-31 21:16:24 -0300

    Commit inicial

---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+    <div class="home">
+        <h1>hello, welcome to my page!</h1>
+        <p>This is the main content area of the application.</p>
+    </div>
+</template>
